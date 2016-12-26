2174778b2f8dd6fc2eb132e85fcbd598f5f78416
Rock <rockrong1204@aliyun.com>
2016-12-26 14:19:05 +0800

    绘制登陆页面

--- vmovie/src/main/java/com/rock/vmovie/ui/login/activity/LoginActivity.java
+++ vmovie/src/main/java/com/rock/vmovie/ui/login/activity/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.rock.vmovie.ui.login.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.rock.teachlibrary.base.BaseActivity;
 import com.rock.vmovie.R;
 
